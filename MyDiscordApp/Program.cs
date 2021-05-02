bc1b9374136265a4d8c1e6837bcf5ffd0837ac09
JamesJoker <jocularjoker@gmail.com>
2021-05-02 20:04:52 +0800

    First commit the Bot can run.

---
+++ MyDiscordApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MyDiscordApp.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace MyDiscordApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        private async Task MainAsync()
+        {
+            await Task.Delay(-1);
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureHostConfiguration(config =>
+                {
+                    config.AddJsonFile("appsetting.json");
+                })
+                .ConfigureLogging(log => 
+                {
+
+                })
+                .ConfigureServices((hostcontent, service) =>
+                {
+                    service.AddHostedService<CommandHandler>()
+                           .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+                           {
+                               GatewayIntents = GatewayIntents.Guilds
+                           }));
+                });
+        }
+    }
+}
---
+++ MyDiscordApp/Services/CommandHandler.cs
@@ -0,0 +1,70 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MyDiscordApp.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyDiscordApp.Services
+{
+    public class CommandHandler : IHostedService
+    {
+        private readonly CommandService _commands;
+        private readonly DiscordSocketClient _discord;
+        private readonly IConfiguration _config;
+        private readonly IServiceProvider _services;
+
+        public CommandHandler(IConfiguration config, IServiceProvider services)
+        {
+            _config = config;
+            _services = services;
+            _commands = new CommandService();
+            _discord = new DiscordSocketClient();
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+            await _discord.LoginAsync(Discord.TokenType.Bot, _config["Token"]);
+            await _discord.StartAsync();
+
+
+            _discord.Ready += OnReady;
+            _discord.MessageReceived += OnReceived;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await InitializeAsync();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Shut down!");
+            return Task.CompletedTask;
+        }
+
+        private Task OnReady()
+        {
+            Console.WriteLine($"{_discord.CurrentUser.Username} is login!");
+            return Task.CompletedTask;
+        }
+
+        private async Task OnReceived(SocketMessage arg)
+        {
+            if(!(arg is SocketUserMessage message)) return;
+
+            var argPos = 0;
+            if (!message.HasCharPrefix('!', ref argPos)) return;
+            var context = new SocketCommandContext(_discord, message);
+            await _commands.ExecuteAsync(context, argPos, _services);
+        }
+    }
+}
